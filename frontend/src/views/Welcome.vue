4a2b652b950db98ad33c68f1afd376e7a61616ed
ThisSeanZhang <thisseanzhang@gmail.com>
2023-03-10 23:27:04 +0800

    fix: Fix buttons on small screens

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -3,7 +3,7 @@ import { NScrollbar, UploadFileInfo, useNotification } from 'naive-ui';
 import { ref} from 'vue'
 import api from '../api'
 import { ChatReq, Message, ExhibitMessage, ChatResp, RespChoice, RespUsage } from '../api/chat';
-import { Delete, Copy } from '@vicons/carbon'
+import { Delete, Copy, SendAlt, Restart, Clean, DocumentExport, DocumentImport } from '@vicons/carbon'
 import { AxiosError } from 'axios';
 
 import useKeyStore from '../store/key'
@@ -182,14 +182,14 @@ function handleUpdateValue (value: number | null) {
 <div style="height: 100%; position: relative">
 <n-layout position="absolute">
   <n-layout-header style="height: 64px; padding: 5px;">
-    <n-grid :cols="5" style="height: 64px;" :x-gap="5">
-      <n-gi :span="1" style="display: flex; justify-content: center;align-items: center;">
+    <n-grid cols="1 400:2 1000:5" style="height: 64px;" :x-gap="5">
+      <n-gi span="0 400:1 1000:1" style="display: flex; justify-content: center;align-items: center;">
         <n-input-group >
             <n-input-group-label>API Key</n-input-group-label>
             <n-select v-model:value="select_index" @update:value="handleUpdateValue" :options="keyStore.all_selects" />
           </n-input-group>
       </n-gi>
-      <n-gi :span="1" style="display: flex; justify-content: center;align-items: center;">
+      <n-gi span="1 400:1 1000:1" style="display: flex; justify-content: center;align-items: center;">
         <n-input-group-label>模型温度(0 ~ 20)</n-input-group-label>
         <n-input-number
           v-model:value="temperature"
@@ -198,7 +198,7 @@ function handleUpdateValue (value: number | null) {
           :max="20"
         />
       </n-gi>
-      <n-gi :span="1" style="display: flex; justify-content: center;align-items: center;">
+      <n-gi span="0 400:0 1000:1" style="display: flex; justify-content: center;align-items: center;">
         <n-input-group>
           <n-input-group-label>模型选择</n-input-group-label>
           <n-select
@@ -209,13 +209,13 @@ function handleUpdateValue (value: number | null) {
           />
         </n-input-group>
       </n-gi>
-      <n-gi :span="2" style="display: flex; justify-content: center;align-items: center;">
+      <n-gi span="0 400:0 1000:2" style="display: flex; justify-content: center;align-items: center;">
         <n-input v-model:value="promptStore.current.value" type="textarea" rows="2" />
       </n-gi>
     </n-grid>
   </n-layout-header>
   <!-- head end -->
-  <n-layout-content style="height: calc(100% - 164px)" content-style="padding: 24px;" :native-scrollbar=false >
+  <n-layout-content style="height: calc(100% - 144px)" content-style="padding: 24px;" :native-scrollbar=false >
     <n-scrollbar ref="chat_context">
     <n-thing content-indented  v-for="(each, index) of history" style="margin: 5px 0px; border: 0 solid">
       <template #avatar>
@@ -240,8 +240,74 @@ function handleUpdateValue (value: number | null) {
     </n-scrollbar>
   </n-layout-content>
   <!-- content end -->
-  <n-layout-footer position="absolute" style="height: 100px; padding: 10px;">
-    <n-grid :cols="20" style="height: 64px;" :x-gap="5">
+  <n-layout-footer position="absolute" style="height: 80px; padding: 10px;">
+    <n-grid cols="10 1000:20" :x-gap="5">
+      <n-gi span="8 1000:16">
+        <n-input @keyup.enter="submit()" v-model:value="user_messgae" type="textarea" rows="2" placeholder="请在此处输入对话"/>
+      </n-gi>
+      <!-- when less then 1000px -->
+      <n-gi span="2 1000:0">
+        <n-grid cols="2">
+          <n-gi span="1">
+            <n-spin :show="submitable" style="display: flex; justify-content: center;align-items: center;">
+              <n-space vertical justify="space-between" align="center">
+                <n-button type="primary" text style="font-size: 25px" :disabled="submitable" @click="submit()">
+                  <template #icon><n-icon><SendAlt /></n-icon></template>
+                </n-button>
+                <n-button type="info" text style="font-size: 25px" :disabled="submitable" @click="submit(true)">
+                  <template #icon><n-icon><Restart /></n-icon></template>
+                </n-button>
+              </n-space>
+            </n-spin>
+          </n-gi>
+          <n-gi span="1">
+            <n-space vertical justify="space-between" align="center">
+              <n-popconfirm :show-icon="false" negative-text="取消" positive-text="确定" @positive-click="clean_history()">
+                <template #trigger>
+                  <n-button type="error" text style="font-size: 20px"><n-icon><Clean /></n-icon></n-button>
+                </template>
+                确认清空所有会话记录?
+              </n-popconfirm>
+            </n-space>
+          </n-gi>
+        </n-grid>
+      </n-gi>
+      <!-- when more then 1000px -->
+      <n-gi span="0 1000:4">
+        <n-grid cols="3">
+          <n-gi span="1">
+            <n-spin :show="submitable" style="display: flex; justify-content: center;align-items: center;">
+              <n-space vertical justify="space-between" align="center">
+                <n-button type="primary" text style="font-size: 25px" :disabled="submitable" @click="submit()">
+                  <template #icon><n-icon><SendAlt /></n-icon></template>
+                </n-button>
+                <n-button type="info" text style="font-size: 25px" :disabled="submitable" @click="submit(true)">
+                  <template #icon><n-icon><Restart /></n-icon></template>
+                </n-button>
+              </n-space>
+            </n-spin>
+          </n-gi>
+          <n-gi span="1">
+            <n-space vertical justify="space-between" align="center">
+              <n-popconfirm :show-icon="false" negative-text="取消" positive-text="确定" @positive-click="clean_history()">
+                <template #trigger>
+                  <n-button type="error" text style="font-size: 20px"><n-icon><Clean /></n-icon></n-button>
+                </template>
+                确认清空所有会话记录?
+              </n-popconfirm>
+            </n-space>
+          </n-gi>
+          <n-gi span="1">
+            <n-space vertical justify="space-between" align="center">
+              <n-button type="success" text @click="export_txt()"><n-icon><DocumentExport /></n-icon></n-button>
+              <n-upload :show-file-list="false" @change="fileChange" ><n-button type="warning" text><n-icon><DocumentImport /></n-icon></n-button></n-upload>
+            </n-space>
+          </n-gi>
+        </n-grid>
+      </n-gi>
+    </n-grid>
+
+    <!-- <n-grid :cols="20" style="height: 64px;" :x-gap="5">
       <n-gi :span="16" style="display: flex; justify-content: center;align-items: center;">
         <n-input @keyup.enter="submit()" v-model:value="user_messgae" type="textarea" rows="2" placeholder="请在此处输入对话"/>
       </n-gi>
@@ -275,11 +341,12 @@ function handleUpdateValue (value: number | null) {
           </n-upload>
         </n-space>
       </n-gi>
-    </n-grid>
+    </n-grid> -->
   </n-layout-footer>
 </n-layout>
 </div>
 </template>
 
 <style scoped>
+
 </style>
